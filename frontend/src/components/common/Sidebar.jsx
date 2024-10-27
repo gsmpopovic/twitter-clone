dccc8d30d0688276d596ac2e7293c4d049dccd11
George Popovic <georgesmpopovic@gmail.com>
2024-10-27 10:25:24 -0400

    change icon dashboard

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -2,7 +2,7 @@ import XSvg from "../svgs/X";
 
 import { MdHomeFilled } from "react-icons/md";
 import { IoNotifications } from "react-icons/io5";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaRegChartBar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -74,7 +74,7 @@ const Sidebar = () => {
 							to={`/dashboard`}
 							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
-							<FaUser className='w-6 h-6' />
+							<FaRegChartBar className='w-6 h-6' />
 							<span className='text-lg hidden md:block'>Dashboard</span>
 						</Link>
 					</li>
